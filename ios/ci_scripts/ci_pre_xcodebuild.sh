5425c32e9bfffdb662727be632b2f9c71bbf1aa8
Joonas Moilanen <joonas.moilanen@codemate.com>
2025-12-02 13:41:04 +0100

    Flutter download added to pre-build script

--- ios/ci_scripts/ci_pre_xcodebuild.sh
+++ ios/ci_scripts/ci_pre_xcodebuild.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
+set -e
 
-# Navigate to repo root
 cd "$XCS_SOURCE_DIR"
 
-# Fetch Flutter dependencies
+# 1. Download Flutter SDK
+if [ ! -d "$XCS_SOURCE_DIR/flutter_sdk" ]; then
+  git clone -b stable https://github.com/flutter/flutter.git flutter_sdk
+fi
+export PATH="$XCS_SOURCE_DIR/flutter_sdk/bin:$PATH"
+
+# 2. Fetch Flutter dependencies
 flutter pub get
 
-# Move to iOS folder and install Pods
+# 3. iOS Pods
 cd ios
 pod install --repo-update
