787b4747d65934c15707f76ec955a3aa9d011ba3
Joonas Moilanen <joonas.moilanen@codemate.com>
2025-12-03 18:12:47 +0100

    build number

--- ios/ci_scripts/ci_post_clone.sh
+++ ios/ci_scripts/ci_post_clone.sh
@@ -23,4 +23,30 @@ brew install cocoapods
 # Install CocoaPods dependencies.
 cd ios && pod install # run `pod install` in the `ios` directory.
 
+cd ..
+
+# -----------------------------
+# Set version/build from pubspec.yaml
+# -----------------------------
+PUBSPEC_VERSION=$(grep '^version:' pubspec.yaml | awk '{print $2}') 
+# Example: 1.0.4+45
+
+# Split version and build
+IFS='+' read -r FLUTTER_VERSION FLUTTER_BUILD <<< "$PUBSPEC_VERSION"
+
+echo "Setting version $FLUTTER_VERSION and build $FLUTTER_BUILD in Info.plist"
+
+# Update iOS Info.plist
+cd ios
+/usr/libexec/PlistBuddy -c "Set :CFBundleShortVersionString $FLUTTER_VERSION" "Runner/Info.plist"
+/usr/libexec/PlistBuddy -c "Set :CFBundleVersion $FLUTTER_BUILD" "Runner/Info.plist"
+cd ..
+
+# -----------------------------
+# Optional: Build IPA (if not done in workflow)
+# -----------------------------
+# flutter build ipa --export-options-plist=ios/ExportOptions.plist
+
+exit 0
+
 exit 0
